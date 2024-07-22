c4431773f71dc97d360639a88eb3cd0427a8d02b
Sricharan Guddanti <sriguddanti@gmail.com>
2024-07-22 02:13:14 -0700

    added checkbox

--- frontend/components/TaskItem.tsx
+++ frontend/components/TaskItem.tsx
@@ -1,43 +1,53 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { Checkbox } from 'react-native-paper';
+import { View, Text, StyleSheet, Button, Switch } from 'react-native';
+import { CheckBox } from '@rneui/themed';
 
 interface TaskItemProps {
-    task: {
-      id: string;
-      description: string;
-      completed: boolean;
-    };
-    onToggleTaskCompletion: (taskId: string) => void;
-    onDeleteTask: (taskId: string) => void;
+  task: {
+    id: string;
+    description: string;
+    completed: boolean;
+  };
+  onToggleTaskCompletion: (taskId: string) => void;
+  onDeleteTask: (taskId: string) => void;
 }
 
 const TaskItem: React.FC<TaskItemProps> = ({ task, onToggleTaskCompletion, onDeleteTask }) => {
-    return (
-        <View style={styles.taskContainer}>
-        <Checkbox
-            status={task.completed ? 'checked' : 'unchecked'}
+  return (
+    <View style={styles.taskContainer}>
+        <CheckBox
+            checked={task.completed}
             onPress={() => onToggleTaskCompletion(task.id)}
+            iconType="material-community"
+            checkedIcon="checkbox-marked"
+            uncheckedIcon="checkbox-blank-outline"
+            checkedColor='black'
+            size={30}
         />
-        <Text style={[styles.taskDescription, task.completed && styles.completedTask]}>
-            {task.description}
-        </Text>
-        <Button title="Delete" onPress={() => onDeleteTask(task.id)} />
-        </View>
-    );
+      {/* <CustomCheckbox
+        checked={task.completed}
+        onPress={() => onToggleTaskCompletion(task.id)}
+      /> */}
+      <Text style={[styles.taskDescription, task.completed && styles.completedTask]}>
+        {task.description}
+      </Text>
+      <Button title="Delete" onPress={() => onDeleteTask(task.id)} color="#ff0000" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   taskContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 10,
+    padding: 0,
     borderBottomColor: '#ddd',
     borderBottomWidth: 1,
   },
   taskDescription: {
-    marginLeft: 10,
+    marginLeft: 0,
     fontSize: 16,
+    flex: 1,
   },
   completedTask: {
     textDecorationLine: 'line-through',
